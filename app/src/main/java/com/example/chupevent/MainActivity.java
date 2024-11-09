772dbf6145c7538dd60f2f56acc62a6ac2f6eb4b
Yong Choon Kit <raphaelxkit@gmail.com>
2024-11-09 08:10:47 +0800

    CK-Bottom Navigation

---
+++ app/src/main/java/com/example/chupevent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.chupevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.chupevent.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.search) {
+                replaceFragment(new SearchFragment());
+            } else if (itemId == R.id.activity) {
+                replaceFragment(new ActivityFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment).commit();
+    }
+}
