6c645a0eb288201d862d5f6a4f90cc453c1ed985
Yong Choon Kit <raphaelxkit@gmail.com>
2025-01-17 03:26:49 +0800

    CK - User Manual

--- app/src/main/java/com/example/chupevent/ProfileFragment.java
+++ app/src/main/java/com/example/chupevent/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.chupevent;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,54 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment {
 
-    private LinearLayout linearlayout1, linearlayout2, linearlayout3;
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private LinearLayout linearlayout1, linearlayout2, linearlayout3, linearlayout4;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -69,6 +25,7 @@ public class ProfileFragment extends Fragment {
         linearlayout1 = view.findViewById(R.id.ll_edit_profile);
         linearlayout2 = view.findViewById(R.id.ll_change_password);
         linearlayout3 = view.findViewById(R.id.ll_log_out);
+        linearlayout4 = view.findViewById(R.id.ll_user_manual);
 
         // Set OnClickListener for the first linear layout
         linearlayout1.setOnClickListener(new View.OnClickListener() {
@@ -84,6 +41,22 @@ public class ProfileFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), LoginScreen.class); // Replace with your Activity's name
                 startActivity(intent);
+            }
+        });
+
+        linearlayout4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the URL of the YouTube video
+                String url = "https://youtu.be/Jw5nxkk22MU?si=xBd_zDTVnebKJUjk";
+
+                // Create an Intent to open the URL
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+
+                // Start the activity
+                v.getContext().startActivity(intent);
+
             }
         });
 
